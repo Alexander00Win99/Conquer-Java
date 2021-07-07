c6ba82b425414b708ad0046d663e0a61d9c0373b
Alexander00Win99 <Alex-Win10@hotmail.com>
2021-07-07 23:53:39 +0800

    2021-07-07

---
+++ MySpringBoot/springboot-10-shiro-spring/src/main/java/com/conquer_java/springboot/Springboot10ShiroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.conquer_java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot10ShiroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot10ShiroSpringApplication.class, args);
+	}
+
+}
